ce86e9f846aa5374dbb43f2437ffeb0e5ba59a48
MysticalX99 <55935446+MysticalX99@users.noreply.github.com>
2019-10-30 10:56:51 -0400

    Update Player.js

--- MELON MAN/Player.js
+++ MELON MAN/Player.js
@@ -1,7 +1,17 @@
 function temp() {
+  thneed.ySpeed = 5;
+  if(thneed.y + thneed.ySpeed >= canvas.height - thneed.height) {
+    while(thneed.y + 1 < canvas.height - thneed.height) {
+      thneed.y++;
+    }
+    thneed.ySpeed = 0
+    thneed.gravity = 0
+  } 
+  /*
   if(thneed.y >= canvas.height - thneed.height) {
    thneed.ySpeed = 0
    thneed.gravity = 0
    thneed.y = canvas.height - thneed.height
-  }
+  }*/
 }
+
